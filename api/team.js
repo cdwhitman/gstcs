728aa7902960d5c7f694e89661ff91d3bee7e402
ccdwhitman <colewhitman91@gmail.com>
2023-04-24 08:57:57 -0500

    Clean up existing code

--- api/team.js
+++ api/team.js
@@ -1,36 +1,22 @@
-import atimg1 from '/public/images/team/img-1.jpg';
-import atimg2 from '/public/images/team/img-2.jpg';
+import doug from '/public/images/team/doug.jpg';
+import cole from '/public/images/team/cole.jpg';
 import atimg3 from '/public/images/team/img-3.jpg';
 import atimg4 from '/public/images/team/img-4.jpg';
 
 const Team = [
   {
     Id: '1',
-    AtImg: atimg1,
-    slug: 'Jenelia-Orkid',
-    name: 'Jenelia Orkid',
-    title: 'Family Lawyer'
+    AtImg: doug,
+    slug: 'Doug-Whitman',
+    name: 'Doug Whitman',
+    title: 'Founder and CEO'
   },
   {
     Id: '2',
-    AtImg: atimg2,
-    slug: 'David-Harry',
-    name: 'David Harry',
-    title: 'Criminal Lawyer'
-  },
-  {
-    Id: '3',
-    AtImg: atimg3,
-    slug: 'Darothy-Jane',
-    name: 'Darothy Jane',
-    title: 'Business Lawyer'
-  },
-  {
-    Id: '4',
-    AtImg: atimg4,
-    slug: 'John-Albart',
-    name: 'John Albart',
-    title: 'Real Estate Lawyer'
+    AtImg: cole,
+    slug: 'Cole-Whitman',
+    name: 'Cole Whitman',
+    title: 'Tech Support'
   }
 ];
 
--- components/about/index.js
+++ components/about/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import abimg from '/public/images/about.jpg';
+import abimg from '/public/images/doug-profile.jpg';
 import sign from '/public/images/signeture.png';
 
 const About = (props) => {
@@ -17,23 +17,49 @@ const About = (props) => {
             <div className='col-lg-7 col-md-12 col-12'>
               <div className='wpo-about-content'>
                 <div className='wpo-section-title'>
-                  <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+                  <h2>About info written for me by AI.</h2>
                 </div>
                 <div className='wpo-about-content-inner'>
                   <p>
-                    There are many variations of passages of Lorem Ipsum
-                    available, but the majority have suffered alteration in some
-                    form, by injected humour, or randomised words which don't
-                    look even slightly believable and going through the cites of
-                    the word in classNameical literature, discovered the
-                    undoubtable source. Lorem Ipsum comes from sections.
+                    Doug Whitman is a seasoned professional with over 26 years
+                    of experience leading global trade compliance teams for
+                    major corporations. Dale brings extensive knowledge and
+                    expertise to our consulting practice, allowing us to deliver
+                    exceptional value to our clients.
                   </p>
                   <p>
-                    Contrary to popular belief, Lorem Ipsum is not simply random
-                    text. It has roots in a piece of classNameical Latin
-                    literature from 45 BC, making it over 2000 years old
-                    consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
+                    As a global trade compliance expert, Dale has worked with
+                    numerous organizations in various industries, providing them
+                    with the guidance and support needed to navigate complex
+                    regulatory requirements and mitigate risk. Dale's experience
+                    spans the entire compliance lifecycle, from developing
+                    compliance programs to implementing compliance procedures
+                    and training teams on best practices.
+                  </p>
+                  <p>
+                    Dale's deep understanding of global trade regulations and
+                    industry standards, combined with his ability to communicate
+                    complex concepts in simple terms, has earned him a
+                    reputation as a trusted advisor to his clients. He has a
+                    track record of delivering successful outcomes for his
+                    clients, enabling them to achieve their compliance
+                    objectives while also driving business growth.
+                  </p>
+                  <p>
+                    Dale's expertise covers a range of areas, including export
+                    controls, sanctions compliance, anti-bribery and corruption,
+                    customs compliance, and more. He stays up-to-date with the
+                    latest regulatory developments and industry trends, ensuring
+                    that our clients benefit from the most current and relevant
+                    advice.
+                  </p>
+                  <p>
+                    Our consulting practice is committed to delivering
+                    outstanding service to our clients, and we are proud to have
+                    Dale as a key member of our team. We invite you to explore
+                    our website and learn more about our services, including how
+                    we can help your organization achieve its compliance
+                    objectives.
                   </p>
                   <div className='signeture'>
                     <h4>Doug Whitman</h4>
