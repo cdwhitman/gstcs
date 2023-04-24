9f62877659f219497071ed4f2c985931d31cfd2e
ccdwhitman <colewhitman91@gmail.com>
2023-04-24 12:28:46 -0500

    fix home nav on mobile

--- components/MobileMenu/index.js
+++ components/MobileMenu/index.js
@@ -5,24 +5,24 @@ const menus = [
   {
     id: 1,
     title: 'Home',
-    link: '/home',
-    submenu: [
-      {
-        id: 11,
-        title: 'Home style 1',
-        link: '/home'
-      },
-      {
-        id: 12,
-        title: 'Home style 2',
-        link: '/home2'
-      },
-      {
-        id: 13,
-        title: 'Home style 3',
-        link: '/home3'
-      }
-    ]
+    link: '/'
+    // submenu: [
+    //   {
+    //     id: 11,
+    //     title: 'Home style 1',
+    //     link: '/home'
+    //   },
+    //   {
+    //     id: 12,
+    //     title: 'Home style 2',
+    //     link: '/home2'
+    //   },
+    //   {
+    //     id: 13,
+    //     title: 'Home style 3',
+    //     link: '/home3'
+    //   }
+    // ]
   },
 
   {
